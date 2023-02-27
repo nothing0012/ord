e109ab0d0921d13ccdf3cef277814d9ca359a876
Rex <rex@magiceden.io>
2023-02-27 15:23:25 -0800

    Add new_output_value

--- src/index/updater/inscription_updater.rs
+++ src/index/updater/inscription_updater.rs
@@ -311,6 +311,7 @@ mod stream {
     tx_value: u64,
     new_satpoint: SatPoint,
     new_owner: Option<Address>,
+    new_output_value: u64,
 
     // create fields
     sat: Option<Sat>,
@@ -340,6 +341,11 @@ mod stream {
           )
           .unwrap(),
         ),
+        new_output_value: tx
+          .output
+          .get(new_satpoint.outpoint.vout as usize)
+          .expect("invalid satpoint")
+          .value,
         tx_value: tx.output.iter().map(|txout: &TxOut| txout.value).sum(),
         sat: None,
         inscription_number: None,
