e0d9f159319a7e86129a4456a3d753fde8781c36
Haozhi Liu <haozhi.liu@magiceden.io>
2023-08-23 16:15:15 +0800

    change rpc to return block rarity chunks instead of total count

--- src/subcommand/server/rpc.rs
+++ src/subcommand/server/rpc.rs
@@ -13,13 +13,13 @@ use std::cmp::{max, min};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct BlockRarities {
-  pub vintage: u64,
-  pub nakamoto: u64,
-  pub first_transaction: u64,
-  pub pizza: u64,
-  pub block9: u64,
-  pub block78: u64,
-  pub palindrome: u64,
+  pub vintage: Vec<(u64, u64)>,
+  pub nakamoto: Vec<(u64, u64)>,
+  pub first_transaction: Vec<(u64, u64)>,
+  pub pizza: Vec<(u64, u64)>,
+  pub block9: Vec<(u64, u64)>,
+  pub block78: Vec<(u64, u64)>,
+  pub palindrome: Vec<(u64, u64)>,
 }
 
 pub(super) async fn handler(
@@ -102,7 +102,7 @@ async fn get_sat_ranges(value: JsonRpcExtractor, index: Arc<Index>) -> JrpcResul
         List::Spent => {}
         List::Unspent(ranges) => {
           for range in ranges {
-            let block_rarities = match get_block_rarities(range.0, range.1 - 1) {
+            let block_rarities = match get_block_rarities(range.0, range.1) {
               Ok(block_rarities) => block_rarities,
               Err(err) => return invalid_params(answer_id, err.to_string()),
             };
@@ -148,105 +148,74 @@ async fn get_sat_ranges(value: JsonRpcExtractor, index: Arc<Index>) -> JrpcResul
 }
 
 fn get_block_rarities(start: u64, end: u64) -> Result<BlockRarities> {
-  let mut vintage = 0;
-  let mut nakamoto = 0;
-  let mut first_transaction = 0;
-  let mut pizza = 0;
-  let mut block9 = 0;
-  let mut block78 = 0;
+  let mut block78_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut block9_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut first_transaction_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut nakamoto_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut palindrome_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut pizza_chunks: Vec<(u64, u64)> = Vec::new();
+  let mut vintage_chunks: Vec<(u64, u64)> = Vec::new();
 
-  let sat_start = Sat(start);
-  let sat_end = Sat(end);
-
-  if start > end {
-    return Err(anyhow!("invalid sat range: start {start} > end {end}"));
+  if start >= end {
+    return Err(anyhow!("invalid sat range: start {start} >= end {end}"));
   }
-  if sat_start.height().n() != sat_end.height().n() {
+
+  let block_height = Sat(start).height().n();
+
+  if block_height != Sat(end - 1).height().n() {
     return Err(anyhow!(
       "invalid sat range: start {start} and end {end} are in different blocks"
     ));
   }
 
-  let block_height = sat_start.height().n();
-
   // ignore sat ranges later than the max pizza block
   if block_height <= MAX_PIZZA_BLOCK_HEIGHT {
     if block_height <= VINTAGE_BLOCK_HEIGHT {
-      vintage += sat_end.n() - sat_start.n() + 1;
+      vintage_chunks.push((start, end));
     }
 
     if NAKAMOTO_BLOCK_HEIGHTS.contains(&block_height) {
-      nakamoto += sat_end.n() - sat_start.n() + 1;
+      nakamoto_chunks.push((start, end));
     }
 
     if block_height == BLOCK9_BLOCK_HEIGHT {
-      block9 = sat_end.n() - sat_start.n() + 1;
-      if sat_start.n() < FIRST_TRANSACTION_SAT_RANGE.1 {
-        first_transaction = min(FIRST_TRANSACTION_SAT_RANGE.1 - 1, sat_end.n())
-          - max(FIRST_TRANSACTION_SAT_RANGE.0, sat_start.n())
-          + 1;
+      block9_chunks.push((start, end));
+      if start < FIRST_TRANSACTION_SAT_RANGE.1 {
+        first_transaction_chunks.push((start, min(FIRST_TRANSACTION_SAT_RANGE.1, end)));
       }
     } else if block_height == BLOCK78_BLOCK_HEIGHT {
-      block78 = sat_end.n() - sat_start.n() + 1;
+      block78_chunks.push((start, end));
     }
 
     if PIZZA_RANGE_MAP.contains_key(&block_height) {
       let pizza_sat_ranges = PIZZA_RANGE_MAP.get(&block_height).unwrap();
-      pizza += count_pizza_sats_in_range(start, end, pizza_sat_ranges);
+      for range in pizza_sat_ranges {
+        if (start >= range.1) || (end <= range.0) {
+          continue;
+        }
+        pizza_chunks.push((max(range.0, start), min(range.1, end)));
+      }
     }
   }
 
   // Assume sat ranges are not too large for most of the cases.
-  let palindrome = if sat_end.n() - sat_start.n() < 1_000_000 {
-    count_palindrome_sats(sat_start.n(), sat_end.n())
-  } else {
-    // this is a rough estimate, but the result is mostly 0 because the palindrome becomes more sparse as the sat number increases.
-    estimate_palindrome_sats(start, end)
-  };
+  if end - start <= 2_000_000 {
+    for i in start..end {
+      if is_palindrome(&i) {
+        palindrome_chunks.push((i, i + 1));
+      }
+    }
+  }
 
   Ok(BlockRarities {
-    vintage,
-    nakamoto,
-    first_transaction,
-    pizza,
-    block9,
-    block78,
-    palindrome,
+    vintage: vintage_chunks,
+    nakamoto: nakamoto_chunks,
+    first_transaction: first_transaction_chunks,
+    pizza: pizza_chunks,
+    block9: block9_chunks,
+    block78: block78_chunks,
+    palindrome: palindrome_chunks,
   })
-}
-
-fn count_palindrome_sats(start: u64, end: u64) -> u64 {
-  let mut count = 0;
-  for i in start..=end {
-    if is_palindrome(&i) {
-      count += 1;
-    }
-  }
-  count
-}
-
-fn estimate_palindrome_sats(start: u64, end: u64) -> u64 {
-  approximate_palindrome_count(end) - approximate_palindrome_count(start)
-}
-
-#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-fn approximate_palindrome_count(range: u64) -> u64 {
-  let num_digits = (range as f64).log10().ceil() as u32;
-  let half_digits = num_digits / 2;
-  let multiplier = 1.0 + (range as f64) / (10u64.pow(num_digits + (num_digits + 1) % 2 - 1) as f64);
-
-  (multiplier * (10u64.pow(half_digits) as f64)) as u64
-}
-
-fn count_pizza_sats_in_range(start: u64, end: u64, ranges: &Vec<(u64, u64)>) -> u64 {
-  let mut count = 0;
-  for range in ranges {
-    if (start >= range.1) || (end < range.0) {
-      continue;
-    }
-    count += min(range.1 - 1, end) - max(range.0, start) + 1
-  }
-  count
 }
 
 #[cfg(test)]
@@ -275,87 +244,82 @@ mod tests {
   }
 
   #[test]
-  fn test_estimate_palindrome_sats() {
-    assert_eq!(estimate_palindrome_sats(0, 1), 1);
-    assert_eq!(estimate_palindrome_sats(0, 10), 10);
-    assert_eq!(estimate_palindrome_sats(0, 100), 19);
-    assert_eq!(estimate_palindrome_sats(0, 1_000), 109);
-    assert_eq!(estimate_palindrome_sats(0, 10_000), 199);
-    assert_eq!(estimate_palindrome_sats(0, 100_000), 1099);
-    assert_eq!(estimate_palindrome_sats(0, 1_000_000), 1999);
-    assert_eq!(estimate_palindrome_sats(0, 10_000_000), 10999);
-    assert_eq!(estimate_palindrome_sats(0, 100_000_000), 19999);
-    assert_eq!(estimate_palindrome_sats(0, 1_000_000_000), 109999);
-    assert_eq!(estimate_palindrome_sats(0, 5_000_000_000), 149999);
-
-    assert_eq!(count_palindrome_sats(0, 1), 2);
-    assert_eq!(count_palindrome_sats(0, 10), 10);
-    assert_eq!(count_palindrome_sats(0, 100), 19);
-    assert_eq!(count_palindrome_sats(0, 1_000), 109);
-    assert_eq!(count_palindrome_sats(0, 10_000), 199);
-    assert_eq!(count_palindrome_sats(0, 100_000), 1099);
-    assert_eq!(count_palindrome_sats(0, 1_000_000), 1999);
-
-    assert_eq!(
-      estimate_palindrome_sats(1_000 * COIN_VALUE, 1_000 * COIN_VALUE + 878_364),
-      0
-    );
-    assert_eq!(
-      count_palindrome_sats(1_000 * COIN_VALUE, 1_000 * COIN_VALUE + 878_364),
-      1
-    );
-
-    assert_eq!(
-      estimate_palindrome_sats(20_000_000 * COIN_VALUE, 20_000_000 * COIN_VALUE + 478_364),
-      0
-    );
-    assert_eq!(
-      count_palindrome_sats(20_000_000 * COIN_VALUE, 20_000_000 * COIN_VALUE + 478_364),
-      1
-    );
-  }
-
-  #[test]
   fn test_get_block_rarities() {
     let mut block_rarities =
       get_block_rarities(460 * COIN_VALUE - 10_000, 460 * COIN_VALUE + 10_000).unwrap();
-    assert_eq!(block_rarities.vintage, 20_001);
-    assert_eq!(block_rarities.nakamoto, 20_001);
-    assert_eq!(block_rarities.first_transaction, 10_000);
-    assert_eq!(block_rarities.block9, 20_001);
-    assert_eq!(block_rarities.block78, 0);
-    assert_eq!(block_rarities.pizza, 0);
-    assert_eq!(block_rarities.palindrome, 2);
+    assert_eq!(
+      block_rarities.vintage,
+      vec![(460 * COIN_VALUE - 10_000, 460 * COIN_VALUE + 10_000)]
+    );
+    assert_eq!(
+      block_rarities.nakamoto,
+      vec![(460 * COIN_VALUE - 10_000, 460 * COIN_VALUE + 10_000)]
+    );
+    assert_eq!(
+      block_rarities.first_transaction,
+      vec![(460 * COIN_VALUE - 10_000, 460 * COIN_VALUE)]
+    );
+    assert_eq!(
+      block_rarities.block9,
+      vec![(460 * COIN_VALUE - 10_000, 460 * COIN_VALUE + 10_000)]
+    );
+    assert_eq!(block_rarities.block78, vec![]);
+    assert_eq!(block_rarities.pizza, vec![]);
+    assert_eq!(
+      block_rarities.palindrome,
+      vec![
+        (45_999_999_954, 45_999_999_955),
+        (46_000_000_064, 46_000_000_065)
+      ]
+    );
 
     block_rarities =
       get_block_rarities(78 * 50 * COIN_VALUE + 10_000, 78 * 50 * COIN_VALUE + 20_000).unwrap();
-    assert_eq!(block_rarities.vintage, 10_001);
-    assert_eq!(block_rarities.nakamoto, 0);
-    assert_eq!(block_rarities.first_transaction, 0);
-    assert_eq!(block_rarities.block9, 0);
-    assert_eq!(block_rarities.block78, 10_001);
-    assert_eq!(block_rarities.pizza, 0);
-    assert_eq!(block_rarities.palindrome, 0);
+    assert_eq!(
+      block_rarities.vintage,
+      vec![(78 * 50 * COIN_VALUE + 10_000, 78 * 50 * COIN_VALUE + 20_000)]
+    );
+    assert_eq!(block_rarities.nakamoto, vec![]);
+    assert_eq!(block_rarities.first_transaction, vec![]);
+    assert_eq!(block_rarities.block9, vec![]);
+    assert_eq!(
+      block_rarities.block78,
+      vec![(78 * 50 * COIN_VALUE + 10_000, 78 * 50 * COIN_VALUE + 20_000)]
+    );
+    assert_eq!(block_rarities.pizza, vec![]);
+    assert_eq!(block_rarities.palindrome, vec![]);
 
     block_rarities = get_block_rarities(
       286 * 50 * COIN_VALUE + 10_000,
       286 * 50 * COIN_VALUE + 20_000,
     )
     .unwrap();
-    assert_eq!(block_rarities.vintage, 10_001);
-    assert_eq!(block_rarities.nakamoto, 10_001);
-    assert_eq!(block_rarities.first_transaction, 0);
-    assert_eq!(block_rarities.block9, 0);
-    assert_eq!(block_rarities.block78, 0);
-    assert_eq!(block_rarities.pizza, 0);
-    assert_eq!(block_rarities.palindrome, 0);
+    assert_eq!(
+      block_rarities.vintage,
+      vec![(
+        286 * 50 * COIN_VALUE + 10_000,
+        286 * 50 * COIN_VALUE + 20_000
+      )]
+    );
+    assert_eq!(
+      block_rarities.nakamoto,
+      vec![(
+        286 * 50 * COIN_VALUE + 10_000,
+        286 * 50 * COIN_VALUE + 20_000
+      )]
+    );
+    assert_eq!(block_rarities.first_transaction, vec![]);
+    assert_eq!(block_rarities.block9, vec![]);
+    assert_eq!(block_rarities.block78, vec![]);
+    assert_eq!(block_rarities.pizza, vec![]);
+    assert_eq!(block_rarities.palindrome, vec![]);
 
     block_rarities = get_block_rarities(204589006000000, 204589046000000).unwrap();
-    assert_eq!(block_rarities.vintage, 0);
-    assert_eq!(block_rarities.nakamoto, 0);
-    assert_eq!(block_rarities.first_transaction, 0);
-    assert_eq!(block_rarities.block9, 0);
-    assert_eq!(block_rarities.block78, 0);
+    assert_eq!(block_rarities.vintage, vec![]);
+    assert_eq!(block_rarities.nakamoto, vec![]);
+    assert_eq!(block_rarities.first_transaction, vec![]);
+    assert_eq!(block_rarities.block9, vec![]);
+    assert_eq!(block_rarities.block78, vec![]);
     /* qualified range
     204589006000000	,	204589008000000 -> 2_000_000
     204589017000000	,	204589019000000 -> 2_000_000
@@ -367,7 +331,20 @@ mod tests {
     204589041000000	,	204589043000000 -> 2_000_000
     204589045000000	,	204589046000000 -> 1_000_000
     */
-    assert_eq!(block_rarities.pizza, 13_000_000);
-    assert_eq!(block_rarities.palindrome, 4);
+    assert_eq!(
+      block_rarities.pizza,
+      vec![
+        (204589006000000, 204589008000000),
+        (204589017000000, 204589019000000),
+        (204589026000000, 204589028000000),
+        (204589029000000, 204589030000000),
+        (204589032000000, 204589033000000),
+        (204589034000000, 204589035000000),
+        (204589037000000, 204589038000000),
+        (204589041000000, 204589043000000),
+        (204589045000000, 204589046000000)
+      ]
+    );
+    assert_eq!(block_rarities.palindrome, vec![]);
   }
 }
