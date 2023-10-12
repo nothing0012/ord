f351da7ba30bcbc7fa6f5c2725e4a35438ea3659
nothing0012 <rex@magiceden.io>
2023-10-12 08:24:18 -0700

    Fix warning

--- src/subcommand/server/rpc.rs
+++ src/subcommand/server/rpc.rs
@@ -228,6 +228,7 @@ fn get_block_rarity_chunks(block_rarity: &BlockRarity, start: u64, end: u64) -> 
   chunks
 }
 
+#[allow(dead_code)]
 fn get_palindromes_from_sat_range(start: u64, end: u64) -> Vec<u64> {
   let mut res = vec![];
   let s = start.to_string();
