3ee0749ba6790e8c39afb8f828a08b95bd929a6a
Michael Yu <michael@magiceden.io>
2023-10-18 19:17:56 -0700

    Additional http headers in traces

--- src/subcommand/server/middleware.rs
+++ src/subcommand/server/middleware.rs
@@ -14,11 +14,34 @@ pub(crate) async fn tracing_layer<B>(request: Request<B>, next: Next<B>) -> Resp
     .unwrap()
     .as_str()
     .to_string();
+  let uri = request.uri();
+  let host = uri.host().unwrap_or_default();
+  let path = uri.path();
+  let path_and_query = uri
+    .path_and_query()
+    .map(|x| x.as_str())
+    .unwrap_or_else(|| "/");
 
   let mut span = tracer.start_with_context(route.clone(), &cx);
   span.set_attribute(Key::new("http.method").string(request.method().as_str().to_string()));
-  span.set_attribute(Key::new("http.target").string(request.uri().path().to_string()));
+  span.set_attribute(Key::new("http.target").string(path.to_string()));
+  span.set_attribute(Key::new("http.host").string(host.to_string()));
   span.set_attribute(Key::new("http.route").string(route));
+  span.set_attribute(Key::new("http.url").string(format!(
+    "{}://{}{}",
+    uri.scheme_str().unwrap_or_default(),
+    host,
+    path_and_query
+  )));
+
+  let useragent = request
+    .headers()
+    .get("user-agent")
+    .map(|x| x.to_str().unwrap_or_default());
+
+  if let Some(useragent) = useragent {
+    span.set_attribute(Key::new("http.useragent").string(useragent.to_string()));
+  }
 
   let response = next.run(request).await;
 
